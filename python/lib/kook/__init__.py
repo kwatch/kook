ac5453e1665c60c364dd178dfea1797f0106b012
makoto kuwata <kwa@kuwata-lab.com>
2009-08-01 02:17:26 +0900

    python: refactor '__init__.py'

--- python/lib/kook/__init__.py
+++ python/lib/kook/__init__.py
@@ -7,16 +7,7 @@
 ###
 
 __RELEASE__ = "$Release: 0.0.0 $".split(' ')[1]
-__all__ = (
-    'KookError', 'KookRecipeError', 'KookCommandError',
-    #'product', 'ingreds', 'byprods', 'coprods', 'priority', 'spices', 'if_exists',
-    #'Recipe', 'TaskRecipe', 'FileRecipe',
-    #'Cookbook', 'Kitchen', # 'Cookable', 'Material', 'Cooking', 'create_context',
-)
-
-import sys, os, re, types
-from kook.utils import *
-import kook.config as config
+__all__ = ('KookError', 'KookRecipeError', 'KookCommandError', )
 
 
 class KookError(Exception):  # StandardError is not available in Python 3.0
