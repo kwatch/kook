8a1bc0f6c1bc0b94e374455f32119f77c1ed35fa
makoto kuwata <kwa@kuwata-lab.com>
2011-08-16 15:14:13 +0900

    python: update python versions in 'Kookbook.py'

--- Kookbook.py
+++ Kookbook.py
@@ -136,7 +136,7 @@ def uninstall(c):
 
 
 vs_path = '/opt/lang/python'
-python_versions = [ '2.5.5', '2.6.6', '2.7.1', '3.0.1', '3.1.3', '3.2.0' ]
+python_versions = [ '2.5.5', '2.6.7', '2.7.2', '3.0.1', '3.1.4', '3.2.1' ]
 
 
 @recipe
